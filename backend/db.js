c21d300ead05592bb7d4ea51a83dba5fa5954497
vaibhav2002desale <vaibhavdesale002@gmail.com>
2026-03-10 17:45:41 +0530

    fix supabase connection

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:YOUR_PASSWORD@db.pdktleoqvqlkyiooqxeu.supabase.co:5432/postgres",
+  connectionString: "postgresql://postgres:vaibhav@4445@db.pdktleoqvqlkyiooqxeu.supabase.co:5432/postgres",
   ssl: {
     rejectUnauthorized: false
   }
